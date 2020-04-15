bb2fa993c332b9e1d98dde73ab81b29d3e3ef847
Eric Lo <lxneng@gmail.com>
2020-04-15 08:06:21 +0800

    set User-Agent

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ async function handleRequest(request) {
   if (!url) {
     return new Response('URL Query Parameter Required!')
   }
-  const rsp = await fetch(url)
+  const rsp = await fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+    }
+  })
   const html = await rsp.text()
   return new Response(html, {
-    headers: { 'content-type': rsp.headers.get('Content-Type')}
+    headers: { 'content-type': rsp.headers.get('Content-Type') }
   })
 }
